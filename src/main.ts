6efef95ab1835c9b6199e1e20e1ff273d8aa6ed6
levinhtuyen <42595840+levinhtuyen@users.noreply.github.com>
2022-05-14 14:59:24 +0700

    add element-plus to project

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import './index.css'
 import { createPinia } from 'pinia';
 import { router } from './router';
 import { i18n } from './plugins/i18n';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const pinia = createPinia();
 createApp(App)
     .use(i18n)
+    .use(ElementPlus)
     .use(firebase)
     .use(pinia)
     .use(router)
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueI18n from '@intlify/vite-plugin-vue-i18n';
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 import { resolve } from 'path';
 
@@ -9,6 +12,12 @@ export default defineConfig({
   plugins: [
     vue(),
     vueI18n({ include: 'locales/**' }),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
